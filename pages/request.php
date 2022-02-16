5ed2ee7d0da855c6a60061eb57c0eb78f670fce3
doniia3zzam <doniaalaa56@gmail.com>
2022-02-16 14:40:16 +0100

    refactor all code

--- pages/request.php
+++ pages/request.php
@@ -1,38 +1,55 @@
 <?php
-echo "all is working";
-echo '<br/>';
-//$data = filter_input_array(INPUT_POST);
-$input = file_get_contents('php://input');
-$encoded_data = json_encode($input);
-file_put_contents('inputs.txt',$encoded_data);
-file_put_contents('post.txt',json_encode($_POST));
-
-echo json_decode($encoded_data);
-echo '<br/>';
-
-// Profile Key (ServerKey)
 $serverKey = "SMJN2GHDM2-JDB2KMKMN2-NZZHLZ69GD";
-// Request body include a signature post Form URL encoded field
-// 'signature' (hexadecimal encoding for hmac of sorted post form fields)
-$signature_fields = filter_input_array(INPUT_POST);
-$requestSignature = $signature_fields["signature"];
-unset($signature_fields["signature"]);
+$request_fields = filter_input_array(INPUT_POST);
 
-// Ignore empty values fields
-$signature_fields = array_filter($signature_fields);
-
-// Sort form fields
-ksort($signature_fields);
-
-// Generate URL-encoded query string of Post fields except signature field.
-$query = http_build_query($signature_fields);
-$signature = hash_hmac('sha256', $query, $serverKey);
-
-if (hash_equals($signature,$requestSignature) === TRUE) {
-    // VALID Redirect
-    echo 'success request';
-}else{
-    // INVALID Redirect
-    echo 'invalid request';
+function verifyRequestSignature($serverKey, $request_fields){
+    // 'signature' (hexadecimal encoding for hmac of sorted post form fields)
+    $requestSignature = $request_fields["signature"];
+    unset($request_fields["signature"]);
+    // Ignore empty values fields
+    $request_fields = array_filter($request_fields);
+    // Sort form fields
+    ksort($request_fields);
+    // Generate URL-encoded query string of Post fields except signature field.
+    $query = http_build_query($request_fields);
+    $signature = hash_hmac('sha256', $query, $serverKey);
+    return hash_equals($signature,$requestSignature); //return bool value
 }
+function invalidRedirect(){
+    /*
+     * 
+     * do your login
+     * 
+     * */
+    echo 'invalid request'; //for example
+}
+function validRedirect(){
+    /*
+     * 
+     * do your login
+     * 
+     * */
+    echo 'success request and redirect is genuine'; //for example
+}
+function handleResponse($serverKey,$request_fields){
+    if (verifyRequestSignature($serverKey,$request_fields)){
+        // VALID Redirect
+        validRedirect();
+    }else{
+        // INVALID Redirect
+        invalidRedirect();
+    }
+}
+//handle the response
+handleResponse($serverKey,$request_fields);
 
+//you can clear the next code
+echo '<br/>';
+echo "All is working";
+echo '<br/>';
+//$input_data = file_get_contents('php://input');
+//$encoded_data = json_encode($input_data);
+//file_put_contents('inputs.txt',$request_fields);
+echo '=========';
+echo '<br/>';
+var_dump($request_fields);
