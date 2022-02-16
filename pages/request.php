6477b6f8b71915541455888eb990a3f36470654c
aseel222 <sasosola88@gmail.com>
2022-02-16 12:35:28 +0200

    handle response

--- pages/request.php
+++ pages/request.php
@@ -1,25 +1,20 @@
 <?php
 echo "all is working";
-$data = filter_input_array(INPUT_POST);
-//var_dump($data);
-//echo '<br/>';
+echo '<br/>';
+//$data = filter_input_array(INPUT_POST);
 $input = file_get_contents('php://input');
 $encoded_data = json_encode($input);
-//var_dump($input);
-
 file_put_contents('inputs.txt',$encoded_data);
-file_put_contents('post.txt',json_encode($data));
+file_put_contents('post.txt',json_encode($_POST));
 
-/*
+echo json_decode($encoded_data);
+echo '<br/>';
+
 // Profile Key (ServerKey)
-$serverKey = "SMJN2GHDM2-JDB2KMKMN2-NZZHLZ69GD"; // Example
-
+$serverKey = "SMJN2GHDM2-JDB2KMKMN2-NZZHLZ69GD";
 // Request body include a signature post Form URL encoded field
 // 'signature' (hexadecimal encoding for hmac of sorted post form fields)
 $signature_fields = filter_input_array(INPUT_POST);
-var_dump($signature_fields);
-
-
 $requestSignature = $signature_fields["signature"];
 unset($signature_fields["signature"]);
 
@@ -31,15 +26,13 @@ ksort($signature_fields);
 
 // Generate URL-encoded query string of Post fields except signature field.
 $query = http_build_query($signature_fields);
-
 $signature = hash_hmac('sha256', $query, $serverKey);
-if (hash_equals($signature, $requestSignature) === TRUE) {
+
+if (hash_equals($signature,$requestSignature) === TRUE) {
     // VALID Redirect
-    // Do your logic
-    echo 'the redirect is valid';
-} else {
+    echo 'success request';
+}else{
     // INVALID Redirect
-    // Log request
-    echo 'something is wrong';
+    echo 'invalid request';
 }
-*/
+
